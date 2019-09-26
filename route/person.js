821851e7a441d41759bdec7a8b1009f3acc4dff9
eldar <123456>
2019-09-26 17:21:35 +0500

    Added controllers and services

--- route/person.js
+++ route/person.js
@@ -4,36 +4,18 @@ const router = new Router({
     prefix: "/persons"
 });
 const personModel = require('../model/person');
-
-const contentType = {"Content-Type": "application/json"};
-
-const personList = [
-    {"id": 1, "name": "Eldar", "surname": "Sagitov", "age": 31},
-    {"id": 2, "name": "Daniya", "surname": "Sagitova", "age": 29},
-    {"id": 3, "name": "Sevilya", "surname": "Sagitova", "age": 6},
-    {"id": 4, "name": "Basir", "surname": "Sagitov", "age": 2},
-    {"id": 5, "name": "Elzara", "surname": "Sagitova", "age": 1},
-];
+const personService = require('../service/person');
+const personController = require('../controller/person');
 
 router
     .get('/:id', async (ctx, next) => {
-        ctx.headers = contentType;
-        console.log(ctx.params.id);
-        ctx.body = personList.filter(value => value.id == ctx.params.id).map(value => value);
+        personController.findById(ctx);
     })
-    .get('/', async (ctx, next) => {
-        let person = personModel.Person({
-            name: "John",
-            surname: "Doe",
-            age: 30
-        });
-        console.log(person);
-        ctx.headers = contentType;
-        ctx.body = personList;
+    .get('/', async (ctx, next) => {        
+        personController.findAll(ctx);
     })
     .post('/', parser(), async (ctx, next) => {
-        ctx.headers = contentType;
-        ctx.body = ctx.request.body;
+        personController.save(ctx);
     });
 
 module.exports = {
